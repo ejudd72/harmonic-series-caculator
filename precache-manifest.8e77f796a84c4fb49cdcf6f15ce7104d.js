b2c67bc54d912b4e1bfbe5ea45fd53ba0ece14c7
Eleanor <e.e.j@live.co.uk>
2020-05-03 15:28:35 +0100

    Updates

---
+++ precache-manifest.8e77f796a84c4fb49cdcf6f15ce7104d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d864175deb573fa28d6ece596ba57f",
+    "url": "/harmonic-series-calculator/index.html"
+  },
+  {
+    "revision": "535db5bbd2a0dca862a5",
+    "url": "/harmonic-series-calculator/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "52ae7f86b3e0d57fa953",
+    "url": "/harmonic-series-calculator/static/css/main.ee868b77.chunk.css"
+  },
+  {
+    "revision": "535db5bbd2a0dca862a5",
+    "url": "/harmonic-series-calculator/static/js/2.567d3256.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/harmonic-series-calculator/static/js/2.567d3256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ae7f86b3e0d57fa953",
+    "url": "/harmonic-series-calculator/static/js/main.8d7a9bce.chunk.js"
+  },
+  {
+    "revision": "1eb25547b8260ec41143",
+    "url": "/harmonic-series-calculator/static/js/runtime-main.019442f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/harmonic-series-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
